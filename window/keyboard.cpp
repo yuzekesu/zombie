d2b980bdba8c746a68cbc0fd937057c017d5b35f
TheVerySmart <yuzekesu@gmail.com>
2025-06-20 17:32:56 +0200

    : added "assert" for checking the range of the visual key in "keyboard.cpp"

--- window/keyboard.cpp
+++ window/keyboard.cpp
@@ -1,22 +1,27 @@
 #include "keyboard.h"
+#include <cassert>
 
 void Keyboard::press(const UCHAR vKey)
 {
+	assert(VK_LBUTTON <= vKey && vKey <= VK_OEM_CLEAR);
 	this->state[vKey] = true;
 }
 
 void Keyboard::release(const UCHAR vKey)
 {
+	assert(VK_LBUTTON <= vKey && vKey <= VK_OEM_CLEAR);
 	this->state[vKey] = false;
 }
 
 bool Keyboard::is_pressing(const UCHAR vKey)
 {
+	assert(VK_LBUTTON <= vKey && vKey <= VK_OEM_CLEAR);
 	return this->state[vKey];
 }
 
 bool Keyboard::is_released(const UCHAR vKey)
 {
+	assert(VK_LBUTTON <= vKey && vKey <= VK_OEM_CLEAR);
 	return !(this->state[vKey]);
 }
 
